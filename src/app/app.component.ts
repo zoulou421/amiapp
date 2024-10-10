c34d712fc82fee31697778b6ce18e9491e027f69
Bonevy BEBY <bonevybeby@gmail.com>
2024-10-10 10:34:26 +0200

    css issue fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,13 @@
 import { AfterViewInit, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { materialize } from 'rxjs';
-import { NavBarComponent } from "./pages/auth/nav-bar/nav-bar.component";
-declare var M: any; // Déclaration de M pour utiliser Materialize
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements AfterViewInit {
-  ngAfterViewInit() {
-    const elems = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(elems); // Initialisation du sidenav
-  }
-  title = 'angular-firebase';
+export class AppComponent {
+    title = 'angular-firebase';
 }
