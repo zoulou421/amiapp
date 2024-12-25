97173797787b0e1d710e88c0e895f2c0a7a54345
Bonevy BEBY <bonevybeby@gmail.com>
2024-12-26 00:43:05 +0100

    update ABOUT

---
+++ src/app/pages/auth/temoignages/temoignages.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-temoignages',
+  standalone: true,
+  imports: [],
+  templateUrl: './temoignages.component.html',
+  styleUrl: './temoignages.component.css'
+})
+export class TemoignagesComponent {
+
+}
